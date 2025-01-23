d8c759e5e4e8a6adbeb915e8423ed847a5f0ba96
wenderu00 <mwmcj@cin.ufpe.br>
2025-01-23 13:20:43 -0300

    do buildings component

--- src/components/Buildings.jsx
+++ src/components/Buildings.jsx
@@ -1,3 +1,8 @@
-export default function Buildings({ buildings }){
-    
+export default function Buildings({ buildings, handleUpgrade }){
+    const buildingsElements = Object.keys(buildings).map( key => (<div>
+        <h3>{key}</h3>
+        <span>Level: {buildings[key].level}</span>
+        <button onClick={() => handleUpgrade(key)}>Upgrade</button>
+    </div>) )
+    return buildingsElements;
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import Buildings from "./Buildings";
 import Time from "./Time";
 export default function Game(){
     const [buildings, setBuildings] = useState({
@@ -15,6 +16,27 @@ export default function Game(){
             daysUpgrading: 0
         }
     })
+    function handleUpgradeBuilding(key){
+        setBuildings(prevBuldings => {
+            const newBuildings = { ...prevBuldings };
+            newBuildings[key].upgrading = true;
+            return newBuildings;
+        })
+    }
+    function upgradeBuildings(buildings){
+        const newBuildings = { ...buildings };
+        Object.keys(newBuildings).forEach( key => {
+            if(newBuildings[key].upgrading && newBuildings[key].daysUpgrading>= newBuildings[key].daysToUpgrade){
+                newBuildings[key].level++;
+                newBuildings[key].upgrading = false;
+                newBuildings[key].daysUpgrading=0;
+            } else if(newBuildings[key].upgrading){
+                newBuildings[key].daysUpgrading++
+            }
+        })
+        return newBuildings;
+    }
+
     const [time, setTime] = useState({
         day: 1,
         month: 0,
@@ -39,11 +61,13 @@ export default function Game(){
     useEffect(() => {
         const interval = setInterval(() => {
             setTime(prevTime => incrementTime(prevTime));
+            setBuildings(prevBuldings => upgradeBuildings(prevBuldings));
         }, 1000)
         return () => clearInterval(interval);
     }, []) 
 
     return (<>
+        <Buildings buildings={buildings} handleUpgrade={handleUpgradeBuilding}/>
         <Time time={time}/>
     </>)
 }
